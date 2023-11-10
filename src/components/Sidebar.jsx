92ccaf835027bd3082d90bc091387e063c29e3ff
Camila Piñero <cmlpr9@gmail.com>
2023-11-10 18:10:08 -0300

    Add functionality to inputs and display in the preview

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,14 @@ import {
 import FormControl from "@mui/material/FormControl";
 import { AddCircleOutlineOutlined } from "@mui/icons-material";
 
-function CvPreview({ fullName, email, phone, address, study }) {
+function CvPreview({
+	fullName,
+	email,
+	phone,
+	address,
+	educationalExperienceArray,
+	workExperienceArray,
+}) {
 	return (
 		<div className="cv">
 			<div className="name">
@@ -38,17 +45,25 @@ function CvPreview({ fullName, email, phone, address, study }) {
 					Educational experience
 				</Typography>
 				<ul>
-					<li>
-						<div className="study-1">
-							<Typography variant="h5">Veterinaria</Typography>
-							<Typography variant="h6">
-								Universidad Nacional de Rio Cuarto
-							</Typography>
-							<Typography variant="h6" gutterBottom>
-								2016-2022
-							</Typography>
-						</div>
-					</li>
+					{educationalExperienceArray && (
+						<>
+							{educationalExperienceArray.map((ee) => (
+								<li key={ee.id}>
+									<div className="study-1">
+										<Typography variant="h5">
+											{ee.title}
+										</Typography>
+										<Typography variant="h6">
+											{ee.school}
+										</Typography>
+										<Typography variant="h6" gutterBottom>
+											{`${ee.dateStart} - ${ee.dateEnd}`}
+										</Typography>
+									</div>
+								</li>
+							))}
+						</>
+					)}
 				</ul>
 			</div>
 			<div className="work-info">
@@ -56,38 +71,32 @@ function CvPreview({ fullName, email, phone, address, study }) {
 					Work experience
 				</Typography>
 				<ul>
-					<li>
-						<div className="work-1">
-							<Typography variant="h5">
-								Atención al cliente
-							</Typography>
-							<Typography variant="h6">
-								Veterinaria Avellaneda
-							</Typography>
-							<Typography variant="h6">2018-2020</Typography>
-							<Typography variant="subtitle1" gutterBottom>
-								subtitle1. Lorem ipsum dolor sit amet,
-								consectetur adipisicing elit. Quos blanditiis
-								tenetur
-							</Typography>
-						</div>
-					</li>
-					<li>
-						<div className="work-2">
-							<Typography variant="h5">
-								Ayudante de veterinaria
-							</Typography>
-							<Typography variant="h6">
-								Veterinaria Avellaneda
-							</Typography>
-							<Typography variant="h6">2020-2023</Typography>
-							<Typography variant="subtitle1" gutterBottom>
-								subtitle1. Lorem ipsum dolor sit amet,
-								consectetur adipisicing elit. Quos blanditiis
-								tenetur
-							</Typography>
-						</div>
-					</li>
+					{workExperienceArray && (
+						<>
+							{workExperienceArray.map((work) => (
+								<li key={work.SideBar}>
+									<div className="work-1">
+										<Typography variant="h5">
+											{work.position}
+										</Typography>
+										<Typography variant="h6">
+											{work.company}
+										</Typography>
+										<Typography variant="h6">
+											{`${work.dateStart} - ${work.dateEnd}
+											`}
+										</Typography>
+										<Typography
+											variant="subtitle1"
+											gutterBottom
+										>
+											{work.responsabilities}
+										</Typography>
+									</div>
+								</li>
+							))}
+						</>
+					)}
 				</ul>
 			</div>
 		</div>
@@ -99,9 +108,26 @@ export default function SideBar() {
 	const [email, setEmail] = useState("mail@mail.com");
 	const [phone, setPhone] = useState("35462258");
 	const [address, setAddress] = useState("Córdoba, Argentina");
-
-	const [study, setStudy] = useState([
-		{ title: "", school: "", dateStart: "", dateEnd: "" },
+	const [educationalExperienceArray, setEducationalExperienceArray] =
+		useState([
+			{
+				id: crypto.randomUUID(),
+				title: "aa",
+				school: "bb",
+				dateStart: "",
+				dateEnd: "",
+			},
+		]);
+	const [workExperienceArray, setWorkExperienceArray] = useState([
+		{
+			id: crypto.randomUUID(),
+			company: "alguna",
+			position: "bb",
+			dateStart: "2010",
+			dateEnd: "1023",
+			responsabilities:
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur",
+		},
 	]);
 
 	function handleFullName(e) {
@@ -118,18 +144,49 @@ export default function SideBar() {
 		setAddress(e.target.value);
 	}
 
-	function handleTitle(e) {
-		return e.target.value;
-	}
+	const addEducationalExperience = () => {
+		setEducationalExperienceArray((prev) => [
+			...prev,
+			{
+				id: crypto.randomUUID(),
+				title: "",
+				school: "",
+				dateStart: "",
+				dateEnd: "",
+			},
+		]);
+	};
+	const handleEducationalExperienceChange = (i, newValue, key) => {
+		let newArr = educationalExperienceArray.map((study) =>
+			educationalExperienceArray.findIndex((s) => s.id === study.id) === i
+				? { ...study, [key]: newValue }
+				: study
+		);
+		setEducationalExperienceArray(newArr);
+	};
 
-	function handleStudy(title, school, dateStart, dateEnd) {
-		let newStudy = {
-			title: title,
-			school: school,
-			dateStart: dateStart,
-			dateEnd: dateEnd,
-		};
-	}
+	const addWorkExperience = () => {
+		setWorkExperienceArray((prev) => [
+			...prev,
+			{
+				id: crypto.randomUUID(),
+				company: "Veterinaria Avellaneda",
+				position: "bb",
+				dateStart: "",
+				dateEnd: "",
+				responsabilities: "",
+			},
+		]);
+	};
+
+	const handleWorkExperienceChange = (i, newValue, key) => {
+		let newArr = workExperienceArray.map((work) =>
+			workExperienceArray.findIndex((s) => s.id === work.id) === i
+				? { ...work, [key]: newValue }
+				: work
+		);
+		setWorkExperienceArray(newArr);
+	};
 
 	const form = (
 		<>
@@ -145,7 +202,7 @@ export default function SideBar() {
 				</Toolbar>
 				<Divider />
 
-				<form noValidate autoComplete="off">
+				<div className="form">
 					<Typography
 						variant="h6"
 						component="div"
@@ -166,7 +223,6 @@ export default function SideBar() {
 							Enter your name
 						</FormHelperText>
 					</FormControl>
-
 					<FormControl>
 						<InputLabel htmlFor="email">Email address</InputLabel>
 						<Input
@@ -180,7 +236,6 @@ export default function SideBar() {
 							Enter your email
 						</FormHelperText>
 					</FormControl>
-
 					<FormControl>
 						<InputLabel htmlFor="phone">Phone number</InputLabel>
 						<Input
@@ -207,6 +262,7 @@ export default function SideBar() {
 							Enter your address
 						</FormHelperText>
 					</FormControl>
+
 					<div className="sectionHeader">
 						<Typography
 							variant="h6"
@@ -222,59 +278,98 @@ export default function SideBar() {
 								width: "40px",
 								height: "40px",
 							}}
+							onClick={addEducationalExperience}
 						>
 							+
 						</IconButton>
 					</div>
+					<ul className="studySection">
+						{educationalExperienceArray.map((ee, i) => (
+							<li key={ee.id}>
+								<div className="sectionBody">
+									<FormControl>
+										<InputLabel htmlFor="school-name">
+											School name
+										</InputLabel>
+										<Input
+											type="text"
+											id="school-name"
+											aria-describedby="helper-school-name"
+											value={ee.school}
+											onChange={(e) =>
+												handleEducationalExperienceChange(
+													i,
+													e.target.value,
+													"school"
+												)
+											}
+										/>
+										<FormHelperText id="helper-school-name">
+											Enter the school name
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<InputLabel htmlFor="title">
+											Title of study
+										</InputLabel>
+										<Input
+											type="text"
+											id="title"
+											aria-describedby="helper-title"
+											value={ee.title}
+											onChange={(e) =>
+												handleEducationalExperienceChange(
+													i,
+													e.target.value,
+													"title"
+												)
+											}
+										/>
+										<FormHelperText id="helper-title">
+											Enter the title of your study
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<Input
+											type="number"
+											id="study-date-start"
+											aria-describedby="helper-study-date-start"
+											value={ee.dateStart}
+											onChange={(e) =>
+												handleEducationalExperienceChange(
+													i,
+													e.target.value,
+													"dateStart"
+												)
+											}
+										/>
+										<FormHelperText id="helper-study-date-start">
+											Enter the start date
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<Input
+											type="number"
+											id="study-date-end"
+											aria-describedby="helper-study-date-end"
+											value={ee.dateEnd}
+											onChange={(e) => {
+												handleEducationalExperienceChange(
+													i,
+													e.target.value,
+													"dateEnd"
+												);
+											}}
+										/>
+										<FormHelperText id="helper-study-date-end">
+											Enter the end date
+										</FormHelperText>
+									</FormControl>
+								</div>
+							</li>
+						))}
+					</ul>
 
-					<div className="sectionBody">
-						<FormControl>
-							<InputLabel htmlFor="school-name">
-								School name
-							</InputLabel>
-							<Input
-								type="text"
-								id="school-name"
-								aria-describedby="helper-school-name"
-							/>
-							<FormHelperText id="helper-school-name">
-								Enter the school name
-							</FormHelperText>
-						</FormControl>
-						<FormControl>
-							<InputLabel htmlFor="title">
-								Title of study
-							</InputLabel>
-							<Input
-								type="text"
-								id="title"
-								aria-describedby="helper-title"
-							/>
-							<FormHelperText id="helper-title">
-								Enter the title of your study
-							</FormHelperText>
-						</FormControl>
-						<FormControl>
-							<Input
-								type="date"
-								id="study-date-start"
-								aria-describedby="helper-study-date-start"
-							/>
-							<FormHelperText id="helper-study-date-start">
-								Enter the start date
-							</FormHelperText>
-						</FormControl>
-						<FormControl>
-							<Input
-								type="date"
-								id="study-date-end"
-								aria-describedby="helper-study-date-end"
-							/>
-							<FormHelperText id="helper-study-date-end">
-								Enter the end date
-							</FormHelperText>
-						</FormControl>
-					</div>
 					<div className="sectionHeader">
 						<Typography
 							variant="h6"
@@ -290,73 +385,125 @@ export default function SideBar() {
 								width: "40px",
 								height: "40px",
 							}}
+							onClick={addWorkExperience}
 						>
 							+
 						</IconButton>
 					</div>
-					<div className="sectionBody">
-						<FormControl>
-							<InputLabel htmlFor="company-name">
-								Company name
-							</InputLabel>
-							<Input
-								type="text"
-								id="company-name"
-								aria-describedby="helper-company-name"
-							/>
-							<FormHelperText id="helper-company-name">
-								Enter the company name
-							</FormHelperText>
-						</FormControl>
-						<FormControl>
-							<InputLabel htmlFor="position">
-								Position title
-							</InputLabel>
-							<Input
-								type="text"
-								id="position"
-								aria-describedby="helper-position"
-							/>
-							<FormHelperText id="helper-position">
-								Enter the title of your position
-							</FormHelperText>
-						</FormControl>
 
-						<FormControl>
-							<Input
-								type="date"
-								id="work-date-start"
-								aria-describedby="helper-work-date-start"
-							/>
-							<FormHelperText id="helper-work-date-start">
-								Enter the start date
-							</FormHelperText>
-						</FormControl>
-						<FormControl>
-							<Input
-								type="date"
-								id="work-date-end"
-								aria-describedby="helper-work-date-end"
-							/>
-							<FormHelperText id="helper-work-date-end">
-								Enter the end date
-							</FormHelperText>
-						</FormControl>
-						<FormControl sx={{ gridRow: "3", gridColumn: "1/-1" }}>
-							<InputLabel htmlFor="responsabilities">
-								Main responsabilities
-							</InputLabel>
-							<Input
-								type="textfield"
-								id="responsabilities"
-								aria-describedby="helper-responsabilities"
-							/>
-							<FormHelperText id="helper-responsabilities">
-								Describe your main rasponsabilities
-							</FormHelperText>
-						</FormControl>
-					</div>
-				</form>
+					<ul className="workSection">
+						{workExperienceArray.map((work, i) => (
+							<li key={work.id}>
+								<div className="sectionBody">
+									<FormControl>
+										<InputLabel htmlFor="company-name">
+											Company name
+										</InputLabel>
+										<Input
+											type="text"
+											id="company-name"
+											aria-describedby="helper-company-name"
+											value={work.company}
+											onChange={(e) =>
+												handleWorkExperienceChange(
+													i,
+													e.target.value,
+													"company"
+												)
+											}
+										/>
+										<FormHelperText id="helper-company-name">
+											Enter the company name
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<InputLabel htmlFor="position">
+											Position title
+										</InputLabel>
+										<Input
+											type="text"
+											id="position"
+											aria-describedby="helper-position"
+											value={work.position}
+											onChange={(e) =>
+												handleWorkExperienceChange(
+													i,
+													e.target.value,
+													"position"
+												)
+											}
+										/>
+										<FormHelperText id="helper-position">
+											Enter the title of your position
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<Input
+											type="number"
+											id="work-date-start"
+											aria-describedby="helper-work-date-start"
+											value={work.dateStart}
+											onChange={(e) =>
+												handleWorkExperienceChange(
+													i,
+													e.target.value,
+													"dateStart"
+												)
+											}
+										/>
+										<FormHelperText id="helper-work-date-start">
+											Enter the start date
+										</FormHelperText>
+									</FormControl>
+									<FormControl>
+										<Input
+											type="number"
+											id="work-date-end"
+											aria-describedby="helper-work-date-end"
+											value={work.dateEnd}
+											onChange={(e) =>
+												handleWorkExperienceChange(
+													i,
+													e.target.value,
+													"dateEnd"
+												)
+											}
+										/>
+										<FormHelperText id="helper-work-date-end">
+											Enter the end date
+										</FormHelperText>
+									</FormControl>
+									<FormControl
+										sx={{
+											gridRow: "3",
+											gridColumn: "1/-1",
+										}}
+									>
+										<InputLabel htmlFor="responsabilities">
+											Main responsabilities
+										</InputLabel>
+										<Input
+											type="textfield"
+											id="responsabilities"
+											aria-describedby="helper-responsabilities"
+											value={work.responsabilities}
+											onChange={(e) =>
+												handleWorkExperienceChange(
+													i,
+													e.target.value,
+													"responsabilities"
+												)
+											}
+										/>
+										<FormHelperText id="helper-responsabilities">
+											Describe your main rasponsabilities
+										</FormHelperText>
+									</FormControl>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</>
 	);
@@ -372,6 +519,10 @@ export default function SideBar() {
 							email={email}
 							phone={phone}
 							address={address}
+							educationalExperienceArray={
+								educationalExperienceArray
+							}
+							workExperienceArray={workExperienceArray}
 						/>
 					</div>
 				</div>
@@ -380,6 +531,4 @@ export default function SideBar() {
 	);
 }
 
-//arreglar que se sale
-//alineacion
-//
+//quedo andando todo teoricamente, agregar funcion descargar pdf
